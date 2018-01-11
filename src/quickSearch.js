b55056082c6bc82d6c15ddbc352a1037b29174c8
Almodovar <Walmodovar@eleadcrm.com>
2018-01-11 14:52:33 -0600

    update pointer and move div outside

--- src/quickSearch.js
+++ src/quickSearch.js
@@ -19,25 +19,27 @@ $(function () {
         window.currentOption.push({ key: id, value: 0 });
 
         var html =
-            '<div class="input-group input-group-sm">' +
-            '<input type="' + settings.searchOptions[0].type + '" id="' + inputId + '" placeholder="' + settings.searchOptions[0].placeholder + '" class="form-control" aria-label="..." />';
+            '<input type="' + settings.searchOptions[0].type + '" id="' + inputId + '" placeholder="' + settings.searchOptions[0].placeholder + '" class="form-control form-control-sm" />';
 
-        if (settings.searchOptions.length > 0) {
+        if (settings.searchOptions.length > 1) {
+            $('#' + id).addClass('input-group');
+            $('#' + id).addClass('input-group-sm');
+
             html +=
                 '<div class="input-group-btn">' +
                 '<button type="button" class="btn btn-default dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&nbsp;<span class="caret"></span></button>' +
                 '<ul class="dropdown-menu dropdown-menu-right">';
 
             for (var i = 0; i < settings.searchOptions.length; i++) {
-                html += '<li><span class="changeOption" data-optionIndex="' + i + '">' + settings.searchOptions[i].placeholder + '</span></li>';
+                html += '<li><span class="changeOption dropdown-item" data-optionIndex="' + i + '">' + settings.searchOptions[i].placeholder + '</span></li>';
             }
 
             html += '</ul></div>';
         }
 
-        this.html(html + '</div>');
+        this.html(html);
 
-        $('.changeOption').click(function () {
+        $('.changeOption').css('cursor', 'pointer').click(function () {
             var optionIndex = $(this).attr('data-optionIndex');
             for (var i = 0; i < window.currentOption.length; i++) {
                 if (window.currentOption[i].key == id) {
@@ -60,6 +62,7 @@ $(function () {
             var searchOption = settings.searchOptions[currentOption];
 
             if (e.which == 13) {
+                e.preventDefault();
                 var value = $(this).val();
                 if (searchOption.type == 'text' || (searchOption.type == 'number' && !isNaN(value))) {
                     $(this).val("");
